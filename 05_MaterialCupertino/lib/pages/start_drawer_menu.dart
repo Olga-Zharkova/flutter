2df350f54aad82a07767537b88ac51073839ab21
Zharkova <62789319+Olga-Zharkova@users.noreply.github.com>
2022-07-06 15:33:03 +0300

    5

--- 05_MaterialCupertino/lib/pages/start_drawer_menu.dart
+++ 05_MaterialCupertino/lib/pages/start_drawer_menu.dart
@@ -7,91 +7,92 @@ class StartDrawerMenu extends StatelessWidget {
   Widget build(BuildContext context) {
     return Drawer(
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          const Padding(padding: EdgeInsets.only(top: 5)),
           const DrawerHeader(
               child: CircleAvatar(
             radius: 70,
             backgroundImage: NetworkImage(
                 'https://www.itl.cat/pngfile/big/186-1861341_widescreen-road-scenery-hd.jpg'),
           )),
-          const Padding(padding: EdgeInsets.only(top: 15)),
-          ListTile(
-              title: Text(
-                'Home',
-                style: TextStyle(fontSize: 22, color: Colors.grey.shade600),
-              ),
-              leading: const Icon(Icons.home, size: 40),
-              trailing: IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.arrow_forward,
-                  size: 40,
+          Column(
+            children: [
+              ListTile(
+                  title: Text(
+                    'Home',
+                    style: TextStyle(fontSize: 22, color: Colors.grey.shade600),
+                  ),
+                  leading: const Icon(Icons.home, size: 40),
+                  trailing: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.arrow_forward,
+                      size: 40,
+                    ),
+                  )),
+              const Padding(padding: EdgeInsets.only(top: 15)),
+              ListTile(
+                title: Text(
+                  'Profile',
+                  style: TextStyle(fontSize: 22, color: Colors.grey.shade600),
                 ),
-              )),
-          const Padding(padding: EdgeInsets.only(top: 15)),
-          ListTile(
-            title: Text(
-              'Profile',
-              style: TextStyle(fontSize: 22, color: Colors.grey.shade600),
-            ),
-            leading: const Icon(Icons.person, size: 40),
-            trailing: IconButton(
-                onPressed: () {},
-                icon: const Icon(Icons.arrow_forward, size: 40)),
-          ),
-          const Padding(padding: EdgeInsets.only(top: 15)),
-          ListTile(
-            title: Text(
-              'Images',
-              style: TextStyle(fontSize: 22, color: Colors.grey.shade600),
-            ),
-            leading: const Icon(Icons.image, size: 40),
-            trailing: IconButton(
-                onPressed: () {},
-                icon: const Icon(Icons.arrow_forward, size: 40)),
+                leading: const Icon(Icons.person, size: 40),
+                trailing: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.arrow_forward, size: 40)),
+              ),
+              const Padding(padding: EdgeInsets.only(top: 15)),
+              ListTile(
+                title: Text(
+                  'Images',
+                  style: TextStyle(fontSize: 22, color: Colors.grey.shade600),
+                ),
+                leading: const Icon(Icons.image, size: 40),
+                trailing: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.arrow_forward, size: 40)),
+              ),
+            ],
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              SizedBox(
-                width: 80,
-                child: FloatingActionButton(
-                  elevation: 1,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(0)),
-                  onPressed: () {},
-                  backgroundColor: Colors.grey.shade200,
-                  child: const Text(
-                    'Выход',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 16,
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(
-                width: 120,
-                child: FloatingActionButton(
-                  elevation: 1,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(0)),
-                  onPressed: () {},
-                  backgroundColor: Colors.grey.shade200,
-                  child: const Text(
-                    'Регистрация',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 16,
-                    ),
-                  ),
-                ),
-              )
+              ButtonDrawer(title: 'Выход', width: 80),
+              ButtonDrawer(title: 'Регистрация', width: 120),
             ],
           ),
+          const Padding(padding: EdgeInsets.only(bottom: 1)),
         ],
       ),
     );
   }
 }
+
+
+class ButtonDrawer extends StatelessWidget {
+  String title;
+  double width;
+  ButtonDrawer({Key? key, required this.title, required this.width}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: width,
+      child: FloatingActionButton(
+        elevation: 1,
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(0)),
+        onPressed: () {},
+        backgroundColor: Colors.grey.shade200,
+        child: Text(
+          title,
+          style: const TextStyle(
+            color: Colors.black,
+            fontSize: 16,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
