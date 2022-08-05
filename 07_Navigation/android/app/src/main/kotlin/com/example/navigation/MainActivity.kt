a075732510b14bc958852f9fff65472038e0c612
Olga Zharkova <olga.534@mail.ru>
2022-08-05 14:46:14 +0300

    првр

--- 07_Navigation/android/app/src/main/kotlin/com/example/navigation/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.navigation
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- 07_Navigation/lib/models/fetch_file.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/services.dart';
-
-Future<String> fetchFileFromAssets(String assetsPath) async {
-  return rootBundle.loadString(assetsPath).then((file) => file);
-}
