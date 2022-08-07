2e2b686c3a5a013ea27ac94220ecf447268ae5f3
Olga Zharkova <olga.534@mail.ru>
2022-08-07 16:22:17 +0300

    edit

--- 07_Navigation_local/lib/pages/albums_page.dart
+++ 07_Navigation_local/lib/pages/albums_page.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:untitled5/components/widgets/containers/etc/artist_card.dart';
 import 'package:untitled5/models/data.dart';
+import 'package:untitled5/models/fetch_file.dart';
 import 'package:untitled5/pages/my_drawer.dart';
 
 class AlbumsPage extends StatefulWidget {
@@ -29,24 +30,57 @@ class _AlbumsPageState extends State<AlbumsPage> {
     _controller.dispose();
   }
 
-  final _artists = json.decode(jsonString);
-
-  //ругается на асинхронную функцию, нельзя так получить данные
-  //final Future<List> _jsonData = json.decode(fetchFileFromAssets('../models/data.json') as String);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor:  Colors.purple.shade50,
-      appBar: AppBar(backgroundColor:  Colors.purple.shade500,),
+      backgroundColor: Colors.purple.shade50,
+      appBar: AppBar(
+        backgroundColor: Colors.purple.shade500,
+      ),
       body: SafeArea(
-        child: ListView.builder(
-          controller: _controller,
-          itemCount: _artists.length,
-          itemBuilder: (context, index) => ArtistCard(
-            nameArtist: _artists[index]['name'],
-            aboutArtist: _artists[index]['about'],
-          ),
+        child: FutureBuilder<String>(
+          future: fetchFileFromAssets('assets/data.json'),
+          builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+            if (snapshot.hasData) {
+              String? jsonData = snapshot.data;
+              return ListView.builder(
+                controller: _controller,
+                itemCount: json.decode(jsonData!).length,
+                itemBuilder: (context, index) => ArtistCard(
+                  nameArtist: json.decode(jsonData)[index]['name'],
+                  aboutArtist: json.decode(jsonData)[index]['about'],
+                ),
+              );
+            } else if (snapshot.hasError) {
+              return Column(
+                children: [
+                  const Icon(
+                    Icons.error_outline,
+                    color: Colors.red,
+                    size: 60,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 16),
+                    child: Text('Error: ${snapshot.error}'),
+                  )
+                ],
+              );
+            } else {
+              return Column(
+                children: const [
+                  SizedBox(
+                    width: 60,
+                    height: 60,
+                    child: CircularProgressIndicator(),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 16),
+                    child: Text('Awaiting result...'),
+                  )
+                ],
+              );
+            }
+          },
         ),
       ),
       drawer: const MyDrawer(),
