e1428e3fffe08299443ee4499f0586ec9a039ea8
Olga Zharkova <olga.534@mail.ru>
2022-08-08 14:53:31 +0300

    78

--- 03_Assets/lib/main.dart
+++ 03_Assets/lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:untitled/carousel_slider_widget.dart';
 import 'package:untitled/my_app.dart';
 
 void main() {
   runApp(
-      const MyApp()
+      const CarouselSliderWidget()
   );
 }
