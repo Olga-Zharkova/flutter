36505e787e85ce7aaad049e90081b508912e6f24
Olga Zharkova <olga.534@mail.ru>
2022-12-13 14:12:06 +0300

    feat: add a starting point

--- 12_flutter_architecture/lib/data/service_provider.dart
+++ 12_flutter_architecture/lib/data/service_provider.dart
@@ -2,14 +2,13 @@ import 'package:flutter_architecture/domain/user_service.dart';
 import 'package:flutter_architecture/mock_server/dummy_service.dart';
 import 'package:get_it/get_it.dart';
 
+class ServiceProvider {
+  static final _getIt = GetIt.instance;
 
-
-class ServiceProvider{
-  static final _getIt = GetIt.I;
   T get<T extends Object>() => _getIt.get<T>();
   static final instance = ServiceProvider();
 
-  void initialize(){
+  void initialize() {
     _getIt.registerLazySingleton<UserService>(() => DummyUserService());
   }
 }
--- 12_flutter_architecture/lib/main.dart
+++ 12_flutter_architecture/lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_architecture/data/service_provider.dart';
 import 'package:flutter_architecture/ui/my_app.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  ServiceProvider().initialize();
 
   runApp(const MaterialApp(home: MyApp()));
 }
