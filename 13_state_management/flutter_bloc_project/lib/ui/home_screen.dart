e4da71aa97542f14738479c6c93f0b6dd0dd61b3
Olga Zharkova <olga.534@mail.ru>
2023-01-15 13:03:32 +0300

    add function

--- 13_state_management/flutter_bloc_project/lib/ui/home_screen.dart
+++ 13_state_management/flutter_bloc_project/lib/ui/home_screen.dart
@@ -26,7 +26,7 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   void addProduct(BuildContext context) {
-    context.read<CounterBloc>().add(CounterIncrementPressed);
+    context.read<CounterBloc>().add(CounterIncrementPressed());
   }
 
   @override
